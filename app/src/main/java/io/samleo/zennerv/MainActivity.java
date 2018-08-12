244053bedcd7e89446e6aad2e4ce5d9e0847c1b1
Samleo <ec3team@gmail.com>
2018-08-12 20:54:19 +0800

    Added file saving function

--- app/src/main/java/io/samleo/zennerv/MainActivity.java
+++ app/src/main/java/io/samleo/zennerv/MainActivity.java
@@ -1,14 +1,23 @@
 package io.samleo.zennerv;
 
-import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
+
 import android.os.Bundle;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.Bundle;
-import android.view.View;
+
+import android.support.design.widget.FloatingActionButton;
 import android.widget.Button;
+
+import android.view.View;
 import android.widget.ImageView;
+
+import android.os.Environment;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
 
 public class MainActivity extends AppCompatActivity {
     private static final int CAMERA_REQUEST = 1888;
@@ -36,7 +45,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onActivityResult ( int requestCode, int resultCode, Intent data){
         if (requestCode == CAMERA_REQUEST && resultCode == AppCompatActivity.RESULT_OK) {
             Bitmap photo = (Bitmap) data.getExtras().get("data");
+            //Save the photo and export as JPG
+
             this.imageDisplay.setImageBitmap(photo);
+
+            BitmapToJPG(photo);
         }
+    }
+
+    public File BitmapToJPG(Bitmap bmp) {
+        String extStorageDirectory = Environment.getExternalStorageDirectory().toString();
+        String DTG = String.valueOf(System.currentTimeMillis());
+        OutputStream outStream = null;
+        // String temp = null;
+        File file = new File(extStorageDirectory, DTG + ".jpg");
+
+        try {
+            outStream = new FileOutputStream(file);
+            bmp.compress(Bitmap.CompressFormat.JPEG, 80, outStream);
+            outStream.flush();
+            outStream.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+        return file;
     }
 }
